d2769306746984a193243ca32499ea389d1c8a05
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-04-30 16:47:16 -0300

    remove view

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
 
-import { View, TouchableOpacity, Text, SafeAreaView } from "react-native";
+import { TouchableOpacity, Text, SafeAreaView } from "react-native";
 import { Feather } from "@expo/vector-icons";
 
 import styles from "./styles";
