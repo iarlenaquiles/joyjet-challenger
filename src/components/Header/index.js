3315750456d8b365ba7b9f1a601c8a1fe792c186
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-04-30 16:46:20 -0300

    create header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+
+import { View, TouchableOpacity, Text, SafeAreaView } from "react-native";
+import { Feather } from "@expo/vector-icons";
+
+import styles from "./styles";
+
+export default function Header() {
+  const navigation = useNavigation();
+  return (
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity
+        style={styles.containerBtnMenu}
+        onPress={() => navigation.toggleDrawer()}
+      >
+        <Feather name="menu" size={30} />
+      </TouchableOpacity>
+      <Text style={styles.textContainer}>Digital Space</Text>
+    </SafeAreaView>
+  );
+}
---
+++ src/components/Header/styles.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    position: "absolute",
+    flex: 1,
+    top: 0,
+    height: 40,
+    width: "100%",
+    backgroundColor: "#fafafa",
+    justifyContent: "flex-start",
+    alignContent: "center",
+    alignItems: "center",
+    flexDirection: "row"
+  },
+  containerBtnMenu: {
+    marginLeft: 20
+  },
+  textContainer: {
+    color: "#3964de",
+    fontSize: 20,
+    marginLeft: 10
+  }
+});
