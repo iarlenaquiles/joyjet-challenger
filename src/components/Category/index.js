2a80a3401b1c37b6fe71bfd2d35b1110e0e94c3c
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-04-30 18:43:29 -0300

    create category page

---
+++ src/components/Category/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import Item from "../Item";
+import styles from "./styles";
+
+export default function Category({ item }) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.textCategoryContainer}>
+        <Text style={styles.textCategory}>{item.category}</Text>
+      </View>
+
+      {item.items.length > 0 &&
+        item.items.map((item, index) => (
+          <Item key={index} item={item} category={item.category} />
+        ))}
+    </View>
+  );
+}
---
+++ src/components/Category/styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  textCategoryContainer: {
+    height: 50,
+    alignItems: "flex-start",
+    paddingLeft: 10,
+    justifyContent: "center",
+    backgroundColor: "#3694de",
+    width: "100%",
+    zIndex: 2
+  },
+  textCategory: {
+    color: "white"
+  }
+});
