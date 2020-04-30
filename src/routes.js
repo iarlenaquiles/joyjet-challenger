110fd92fb1b4b4d349126bcd814aad2a87b9efde
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-04-30 14:12:14 -0300

    create routes

---
+++ src/routes.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+
+const AppStack = createStackNavigator();
+const AppDrawer = createDrawerNavigator();
+
+const HomeNavigator = () => (
+  <AppStack.Navigator initialRouteName="Home" headerMode="none">
+    <AppStack.Screen name="HomeScreen" component={HomeScreen} />
+    <AppStack.Screen name="ItemScreen" component={ItemScreen} />
+  </AppStack.Navigator>
+);
+
+const FavoriteNavigator = () => (
+  <AppStack.Navigator initialRouteName="Favorite" headerMode="none">
+    <AppStack.Screen name="FavoriteScreen" component={FavoriteScreen} />
+    <AppStack.Screen name="ItemScreen" component={ItemScreen} />
+  </AppStack.Navigator>
+);
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <AppDrawer.Navigator
+        initialRouteName="Home"
+        drawerPosition="right"
+        drawerContent={props => <CustomDrawer {...props} />}
+      >
+        <AppDrawer.Screen name="Home" component={HomeNavigator} />
+        <AppDrawer.Screen name="Favorites" component={FavoriteNavigator} />
+      </AppDrawer.Navigator>
+    </NavigationContainer>
+  );
+}
