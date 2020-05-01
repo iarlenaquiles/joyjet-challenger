a67e307f3f55488beb847505a331b32a35df7961
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-05-01 02:08:35 -0300

    main file

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import React from "react";
 import "./src/config/ReactotronConfig";
+import { AsyncStorage } from "react-native";
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
 import { StatusBar } from "react-native";
+import thunk from "redux-thunk";
 
-import { store, persistor } from "./src/store";
-
+import { persistStore, persistReducer } from "redux-persist";
 import Routes from "./src/routes";
+import reducers from "./src/redux/favorites";
+import { createStore, applyMiddleware } from "redux";
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+const store = createStore(persistedReducer, applyMiddleware(thunk));
+
+let persistor = persistStore(store);
 
 export default function Index() {
   return (
